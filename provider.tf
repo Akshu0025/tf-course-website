334ed10f829c40ab8046472c44be56ca0e3a7a5f
Akshu0025 <akshagna.p@gmail.com>
2024-04-10 10:01:23 +0000

    ran terraform fmt

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform {
-   backend "gcs" {
-    bucket  = "roidtc-240408-user518-tf-state"
-    prefix  = "terraform/state"
+  backend "gcs" {
+    bucket = "roidtc-240408-user518-tf-state"
+    prefix = "terraform/state"
   }
 
   required_providers {
